79c02d1e7f63d6ceb3d138eca4fc31c14f7f30c6
Marjan <marjan.stojanovic@prip.ne>
2016-09-21 19:25:53 +0200

    adding first test.

---
+++ test/test_error.py
@@ -0,0 +1,12 @@
+import unittest
+
+class ErrorTest(unittest.TestCase):
+
+
+    def testName(self):
+        assert False
+
+
+if __name__ == "__main__":
+    # import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
