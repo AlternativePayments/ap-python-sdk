41de276fdc58536e6062bf9e1055d9cfaf6cbe34
Marjan <marjan.stojanovic@prip.ne>
2016-09-21 19:56:24 +0200

    First error test.

--- src/ap_python_sdk/test/test_error.py
+++ src/ap_python_sdk/test/test_error.py
@@ -1,10 +1,13 @@
+from ap_python_sdk.error import AlternativePaymentsError
 import unittest
+
 
 class ErrorTest(unittest.TestCase):
 
 
-    def testName(self):
-        assert False
+    def test_alternative_payments_error(self):
+        err = AlternativePaymentsError("Unexpected error communicating with Alternative Payments.")
+        assert("Unexpected error communicating with Alternative Payments.", err.message)
 
 
 if __name__ == "__main__":
