8a0526ff95f510305aba46a0a34849cd0b2429cd
Marjan <marjan.stojanovic@prip.ne>
2016-09-21 20:56:08 +0200

    T

--- src/ap_python_sdk/test/test_error.py
+++ src/ap_python_sdk/test/test_error.py
@@ -1,5 +1,6 @@
 from ap_python_sdk.error import AlternativePaymentsError, APIError, \
     InvalidParameterError
+from numpy.ma.testutils import assert_equal
 import unittest
 
 
@@ -7,15 +8,15 @@ class ErrorTest(unittest.TestCase):
 
     def test_alternative_payments_error(self):
         err = AlternativePaymentsError("Unexpected error communicating with Alternative Payments.")
-        assert("Unexpected error communicating with Alternative Payments.", err.message)
+        assert_equal(err.message, "Unexpected error communicating with Alternative Payments.")
 
     def test_api_error(self):
         err = APIError("Unexpected error communicating with Alternative Payments.")
-        assert("Unexpected error communicating with Alternative Payments.", err.message)
+        assert_equal(err.message, "Unexpected error communicating with Alternative Payments.")
 
     def test_invalid_parameter_error(self):
-        err = InvalidParameterError("Invalid parameter error")
-        assert("Invalid parameter error", err.message)
+        err = InvalidParameterError("Invalid parameter name error.", "name")
+        assert("Invalid parameter name.", err.message)
 
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'Test.testName']
